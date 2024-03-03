505fb2dd3e93b006585c9dc9c221e23a11b728fb
Marker-bit <maarrrkkkk@ya.ru>
2024-03-03 14:14:37 +0300

    update some code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,13 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { useTime } from "@/lib/use-time";
-import { AnimatePresence, motion } from "framer-motion";
+import {
+  AnimatePresence,
+  motion,
+  useMotionValue,
+  useSpring,
+  useTransform,
+} from "framer-motion";
 import { Palette, Settings } from "lucide-react";
 import { useTheme } from "next-themes";
 import {
@@ -19,18 +25,50 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { DM_Sans, Inter, Nunito, Rubik } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
+// const fonts = [Rubik, Inter, Nunito, DM_Sans];
+
+const font = DM_Sans({ subsets: ["latin"] });
 
 export default function Home() {
   const { theme } = useTheme();
   const time = useTime();
-  const nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":"];
+  const alphabet =
+    "abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя".split("");
+  const nums = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    ":",
+    ".",
+    ...alphabet,
+    " ",
+  ];
   const [color, setColor] = useLocalStorage<NamedColor>(
     "color",
     theme === "dark" ? "wheat" : "black"
   );
+  const [weight, setWeight] = useState(400);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center transition-all">
+    <main
+      className="flex min-h-screen flex-col items-center justify-center transition-all"
+      onMouseMove={(evt) => {
+        const x = evt.clientX;
+        const percent = (x / window.innerWidth) * 1000;
+        setWeight(percent);
+      }}
+    >
       <div className="absolute top-5 left-5 flex gap-1">
         <ModeToggle currentColor={color} setColor={setColor} />
         <Popover>
@@ -61,49 +99,50 @@ export default function Home() {
         </Dialog>
       </div>
       <motion.p
-        className="bg-gradient-to-b from-zinc-50 via-zinc-200 to-zinc-50 dark:from-zinc-950 dark:via-zinc-800 dark:to-zinc-950 p-8 rounded-3xl font-black text-9xl overflow-hidden transition-all cursor-default max-sm:max-w-full"
+        className={cn(
+          "bg-gradient-to-b from-zinc-50 via-zinc-200 to-zinc-50 dark:from-zinc-950 dark:via-zinc-800 dark:to-zinc-950 p-8 rounded-3xl text-9xl overflow-hidden cursor-default max-sm:max-w-full flex -space-x-1 font-[1000]",
+          font.className
+        )}
         layout
         style={{ color: color }}
       >
-        <motion.div className="flex -space-x-1" layout>
-          {time
-            .toLocaleTimeString()
-            .toString()
-            .split("")
-            .map((currentNum, i) => (
-              <>
-                <div className="relative" key={i}>
-                  <AnimatePresence mode="popLayout">
-                    {nums.map(
-                      (num) =>
-                        currentNum === num && (
-                          <motion.div
-                            initial={{
-                              y: -100,
-                              opacity: 0,
-                              pointerEvents: "none",
-                            }}
-                            animate={{
-                              y: 0,
-                              opacity: 1,
-                              pointerEvents: "auto",
-                            }}
-                            exit={{
-                              y: 100,
-                              opacity: 0,
-                              pointerEvents: "none",
-                            }}
-                            key={num}
-                          >
-                            {num}
-                          </motion.div>
-                        )
-                    )}
-                  </AnimatePresence>
-                </div>
-              </>
-            ))}
-        </motion.div>
+        {/* <motion.div className="flex -space-x-1" layout> */}
+        {time
+          .toLocaleTimeString()
+          .toString()
+          .split("")
+          .map((currentNum, i) => (
+            <div className="relative" key={i}>
+              <AnimatePresence mode="popLayout">
+                {nums.map(
+                  (num) =>
+                    currentNum === num && (
+                      <motion.div
+                        initial={{
+                          y: -100,
+                          opacity: 0,
+                          pointerEvents: "none",
+                        }}
+                        animate={{
+                          y: 0,
+                          opacity: 1,
+                          pointerEvents: "auto",
+                        }}
+                        exit={{
+                          y: 100,
+                          opacity: 0,
+                          pointerEvents: "none",
+                        }}
+                        key={num}
+                      >
+                        {num}
+                      </motion.div>
+                    )
+                )}
+              </AnimatePresence>
+            </div>
+          ))}
+        {/* </motion.div> */}
       </motion.p>
     </main>
   );
